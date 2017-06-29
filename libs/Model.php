d4af95135ad3b61a31c4794cb923b4f1e3509c8e
ahmed_Elkafrawy <ahmed@ahmed.com>
2017-06-29 13:24:11 +0200

    adding database Connection

--- libs/Model.php
+++ libs/Model.php
@@ -5,9 +5,19 @@ class Model {
     public $app;
 
     function __construct() {
-
      
+       require_once 'Config/DatabaseVar.php';
+        
+        try{
+            
+        $conn = new PDO("mysql:host=localhost;dbname=".DBNAME,USERNAME,PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            
+        }
 
+       catch (Exception $ex) {
+            echo "Error in Connection " . $ex.getMessage();
+        }
 
     }
     
